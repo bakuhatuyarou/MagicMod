2d28c7de78bf88e214ca759788e41b666088bb55
bakuhatuyarou <naitou8216@yahoo.co.jp>
2015-08-20 00:43:46 +0900

    Add Block Error

--- src/main/java/MagicMod/MagicModClass.java
+++ src/main/java/MagicMod/MagicModClass.java
@@ -1,9 +1,23 @@
 package MagicMod;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
 
 @Mod(modid="MagicMod",name="MagicMod",version="1.0")
 
 public class MagicModClass {
 
+	public static MagicModClass MagicMod;
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event){
+
+		MagicMod = new MagicModClass();
+
+				GameRegistry.registerBlock(MagicMod, "MagicModClass");
+
 }
+}
+
