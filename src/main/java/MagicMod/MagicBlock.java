b015a60d2d275327865e4d0af252ddd0f3e547ad
Spec <flsoundsonic@gmail.com>
2015-08-20 08:28:17 +0900

    新ブロック「MagicBlock」追加

---
+++ src/main/java/MagicMod/MagicBlock.java
@@ -0,0 +1,84 @@
+package MagicMod;
+
+import java.util.Random;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.IIcon;
+import net.minecraft.world.World;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+public class MagicBlock extends Block {
+
+	@SideOnly(Side.CLIENT)
+	private IIcon TopIcon;
+
+	@SideOnly(Side.CLIENT)
+	private IIcon SideIcon;
+
+	public MagicBlock() {
+
+		super(Material.rock);
+		setCreativeTab(CreativeTabs.tabBlock);/*クリエイティブタブの選択*/
+		setBlockName("magicBlock");/*システム名の設定*/
+		setBlockTextureName("magicmod:MagicBlockCore");/*ブロックのテクスチャの指定(複数指定の場合は消してください)*/
+		/*以下のものは消しても結構です*/
+		setHardness(1.5F);/*硬さ*/
+		setResistance(1.0F);/*爆破耐性*/
+		setStepSound(Block.soundTypeStone);/*ブロックの上を歩いた時の音*/
+		/*setBlockUnbreakable();*//*ブロックを破壊不可に設定*/
+		/*setTickRandomly(true);*//*ブロックのtick処理をランダムに。デフォルトfalse*/
+		/*disableStats();*//*ブロックの統計情報を保存しない*/
+		setLightOpacity(1);/*ブロックの透過係数。デフォルト０（不透過）*/
+		setLightLevel(1.0F);/*明るさ 1.0F = 15*/
+	}
+
+	@Override
+	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float posX, float posY, float posZ){
+		//TODO: ブロックを右クリックした際の動作
+		return true;
+	}
+
+	@Override
+	public void onBlockClicked(World world, int x, int y, int z, EntityPlayer player){
+		//TODO: ブロックを左クリックした際の動作
+	}
+
+	@Override
+	public void onNeighborBlockChange(World world, int x, int y, int z, Block neighborBlock){
+		//TODO: 周囲のブロックが更新された際の動作
+	}
+
+	@Override
+	public int quantityDropped(int meta, int fortune, Random random){
+		//TODO: ドロップするアイテムを返す
+		return quantityDroppedWithBonus(fortune, random);
+	}
+
+	@Override
+	public int quantityDropped(Random random){
+		//TODO: ドロップさせる量を返す
+		return 1;
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void registerBlockIcons(IIconRegister par1IconRegister){
+		this.TopIcon = par1IconRegister.registerIcon("magicmod:MagicBlockCore");
+		this.SideIcon = par1IconRegister.registerIcon("magicmod:MagicBlockCore");
+	}
+
+	@SideOnly(Side.CLIENT)
+	public IIcon getIcon(int par1, int par2){
+		if(par1 == 0 || par1 == 1){
+				return TopIcon;
+		}
+		else{
+				return SideIcon;
+		}
+	}
+}
--- src/main/java/MagicMod/MagicModClass.java
+++ src/main/java/MagicMod/MagicModClass.java
@@ -1,23 +1,22 @@
 package MagicMod;
 
+import net.minecraft.block.Block;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
 
-@Mod(modid="MagicMod",name="MagicMod",version="1.0")
-
+@Mod(modid = "MagicMod", name = "MagicMod", version = "1.0")
 public class MagicModClass {
 
-	public static MagicModClass MagicMod;
+	public static Block magicBlock;
 
 	@EventHandler
-	public void preInit(FMLPreInitializationEvent event){
+	public void preInit(FMLPreInitializationEvent event) {
 
-		MagicMod = new MagicModClass();
+		magicBlock = new MagicBlock();
 
-				GameRegistry.registerBlock(MagicMod, "MagicModClass");
+		GameRegistry.registerBlock(magicBlock, "MagicBlock");
 
+	}
 }
-}
-
