d0063c085fa87a2f8cdf9227f5f6c8d58f5a262e
bakuhatuyarou <naitou8216@yahoo.co.jp>
2015-08-21 22:23:59 +0900

    Crash

--- src/main/java/MagicMod/MagicMod.java
+++ src/main/java/MagicMod/MagicMod.java
@@ -1,13 +1,14 @@
 package MagicMod;
 
-import net.minecraft.block.Block;
-import net.minecraft.init.Blocks;
-import net.minecraft.item.ItemStack;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
 
 @Mod(modid = "MagicMod", name = "MagicMod", version = "1.0")
 public class MagicMod {
@@ -27,8 +28,15 @@ public class MagicMod {
 	public void init(FMLInitializationEvent event){
 
 		//レシピ追加
-		GameRegistry.addShapelessRecipe(new ItemStack(magicBlock),
-				new Object[] {Blocks.dirt, Blocks.dirt} );
+		GameRegistry.addRecipe(new ItemStack(magicBlock),
+				"DGD",
+				"GRG",
+				"DGD",
+				"D",Items.diamond,
+				"G",Items.gold_ingot,
+				"R",Blocks.redstone_block
+
+				);
 	}
 
 }
