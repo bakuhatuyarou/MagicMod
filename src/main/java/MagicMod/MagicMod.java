2e8fb00d3c50527e4db0fef593b772ccf3764fb2
bakuhatuyarou <naitou8216@yahoo.co.jp>
2015-08-22 17:00:41 +0900

    Error

--- src/main/java/MagicMod/MagicMod.java
+++ src/main/java/MagicMod/MagicMod.java
@@ -1,17 +1,22 @@
 package MagicMod;
 
-import net.minecraft.block.Block;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.init.Blocks;
-import net.minecraft.init.Items;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
+import net.minecraft.block.Block;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.gen.feature.WorldGenMinable;
+import net.minecraft.world.gen.feature.WorldGenerator;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.terraingen.OreGenEvent;
+import net.minecraftforge.event.terraingen.TerrainGen;
 
 @Mod(modid = "MagicMod", name = "MagicMod", version = "1.0")
 public class MagicMod {
@@ -47,6 +52,14 @@ public class MagicMod {
 
 	@Mod.EventHandler
 	public void init(FMLInitializationEvent event){
+		MinecraftForge.ORE_GEN_BUS.register(this);
+	}
+		＠SubscribeEvent
+		public void generateOrePre(OreGenEvent.Pre event){
+			WorldGenerator bedrockGen = new WorldGenMinable(Blocks.bedrock.getDefaultState(), 9);
+			if(TerrainGen.generateOre(event.world, event.rand,bedrockGen, event.pos, OreGenEvent.GenerateMinable.EventType.CUSTOM))
+				genStandardOre1(event.world, event.pos, 20, bedrockGen, 0, 96, event.rand);
+
 
 		//MagicBlock定型レシピ追加
 		GameRegistry.addRecipe(new ItemStack(magicBlock),
@@ -58,9 +71,11 @@ public class MagicMod {
 				'R',Blocks.redstone_block
 				);
 		GameRegistry.addRecipe(new ItemStack(magicMass),
-				"DD",
-				"DD",
-				'D',Blocks.dirt
+				"MMM",
+				"MDM",
+				"MMM",
+				'D',Items.diamond,
+				'M',magicDust
 				);
-	}
+		}
 }
